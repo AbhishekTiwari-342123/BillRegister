32ba57d6f9639f21e6d853869fd03633e1dbe96f
unknown <abhishektiwari342123@gmail.com>
2022-03-16 17:19:51 +0530

    Account Head Model Created

--- allotment/models.py
+++ allotment/models.py
@@ -1,11 +1,21 @@
 
+from email.header import Header
+from tkinter import CASCADE
 from django.db import models
 # Create your models here
 
+class Major_Head(models.Model):
+    Head_No=models.IntegerField(primary_key=True)
+    Head_Name=models.CharField(default="",max_length=250)
 
-class Alloted_Total(models.Model):
-    Salary_Total_Allotment=models.IntegerField(default=0)
+class Sub_Major(models.Model):
+    Head_No=models.ForeignKey(Major_Head, on_delete=CASCADE)
+    Sub_Head_No=models.ForeignKey()
+    Sub_Head_Name=models.CharField(default="")
 
+
+class Alloted_Salary_Total(models.Model):
+    pass
 
 class Allotment_Salary(models.Model):
     Bill_Number=models.CharField(primary_key=True, max_length=20)
