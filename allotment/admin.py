c536afaabf5ad7f7b7d098cd4b6d07d5536f357a
unknown <abhishektiwari342123@gmail.com>
2022-03-17 17:25:47 +0530

    MinorScheme Model added

--- allotment/admin.py
+++ allotment/admin.py
@@ -3,11 +3,39 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Major_Sub_Head)
-admin.site.register(Minor_Head)
-admin.site.register(Scheme)
-admin.site.register(Sub_Scheme)
-admin.site.register(Object_Head)
-admin.site.register(Salary_Allotment)
-admin.site.register(Salary_Register)
+
+
+class MajorSubHead_Admin(admin.ModelAdmin):
+    list_display=('Head_Code','SubHead_Code','Head_Name','SubHead_Name')
+
+class MinorHead_Admin(admin.ModelAdmin):
+    list_display=('MinorHead_Code','MinorHead_Name','Head_Code')
+
+class Scheme_Admin(admin.ModelAdmin):
+    list_display=('Scheme_Code','Scheme_Name','MinorHead_Code')
+
+class SubScheme_Admin(admin.ModelAdmin):
+    list_display=('SubScheme_Code','SubScheme_Name','Scheme_Code')
+
+class ObjectHead_Admin(admin.ModelAdmin):
+    list_display=('Object_Code','Object_Name','SubScheme_Code')
+
+class SalaryAllotment_Admin(admin.ModelAdmin):
+    list_display=('Object_Code','Allotment_Amount')
+
+class SalaryRegister_Admin(admin.ModelAdmin):
+    list_display=('Bill_Type','Bill_Number','Bill_Date','Description','Bill_Amount','Bill_Expenditure','Bill_Balance','Bill_Remark')
+
+
+class MinorScheme_Admin(models.Model):
+    list_display=('MinorHead_Code','Scheme_Code')
+
+admin.site.register(Major_Sub_Head,MajorSubHead_Admin)
+admin.site.register(Minor_Head,MinorHead_Admin)
+admin.site.register(Scheme,Scheme_Admin)
+admin.site.register(Sub_Scheme,SubScheme_Admin)
+admin.site.register(Object_Head,ObjectHead_Admin)
+admin.site.register(Salary_Allotment,SalaryAllotment_Admin)
+admin.site.register(MinorScheme,MinorScheme_Admin)
+admin.site.register(Salary_Register,SalaryRegister_Admin)
 
--- allotment/models.py
+++ allotment/models.py
@@ -15,10 +15,30 @@ class Major_Sub_Head(models.Model):
     Head_Name=models.CharField(default="",max_length=250)
     SubHead_Name=models.CharField(default="",max_length=250)
 
+    def __str__(self):
+        return str(self.Head_Code)
 
 class Minor_Head(models.Model):
     MinorHead_Code=models.IntegerField(primary_key=True)
+    MinorHead_Name=models.CharField(default="",max_length=250)
     Head_Code=models.ForeignKey(Major_Sub_Head, to_field="Head_Code", on_delete=models.CASCADE)
+
+    
+    def __str__(self):
+        return str(self.MinorHead_Code)
+
+class MinorScheme(models.Model):
+    MinorHead_Code = models.IntegerField()
+    Scheme_Code = models.IntegerField()
+
+    class Meta:
+        unique_together = (('MinorHead_Code', 'Scheme_Code'),)
+
+    def __str__(self):
+        return str(self.MinorHead_Code+"-"+self.Scheme_Code)    
+
+
+
 
 
 # one-many reln between Sub_Major and Schemes, Total Participation on Schemes Side
@@ -28,8 +48,10 @@ class Minor_Head(models.Model):
 class Scheme(models.Model):
     Scheme_Code=models.IntegerField(primary_key=True)
     Scheme_Name=models.CharField(default="",max_length=250)
-    MinorHead_Code=models.ForeignKey(Minor_Head, to_field='MinorHead_Code', on_delete=models.CASCADE)
+    MinorHead_Code=models.ForeignKey(MinorScheme, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.Scheme_Code)
 
 # one-many reln between Scheme and Sub_Scheme, Total Participation on Sub_Scheme Side
 # two tables Scheme(Scheme_Code(key),Scheme_Name); Sub_Scheme(SubScheme_Code(key), SubScheme_Name, Scheme_Code(fkey))
@@ -40,6 +62,10 @@ class Sub_Scheme(models.Model):
     SubScheme_Name=models.CharField(default="",max_length=250)
     Scheme_Code=models.ForeignKey(Scheme, to_field='Scheme_Code', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.SubScheme_Code)
+
+
 # one-many reln between Sub_Scheme and Object_Head, Total Participation on Object_Head Side
 # two tables Sub_Scheme(SubScheme_Code(key),SubScheme_Name); Object_Head(Object_Code(key), Object_Name, SubScheme_Code(fkey))
 # And Sub_Scheme(SubScheme_Code(key),SubScheme_Name) already exists in the Sub_Scheme(SubScheme_Code(key), SubScheme_Name, Scheme_Code(fkey)) table
@@ -49,6 +75,8 @@ class Object_Head(models.Model):
     Object_Name=models.CharField(default="",max_length=250)
     SubScheme_Code=models.ForeignKey(Sub_Scheme, to_field='SubScheme_Code', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.Object_Code)
 
 ########################################################## Scheme Master Table ends ###########################################################
 
@@ -59,6 +87,9 @@ class Object_Head(models.Model):
 class Salary_Allotment(models.Model):
     Object_Code=models.ForeignKey(Object_Head, to_field='Object_Code', unique=True, on_delete=models.CASCADE)
     Allotment_Amount=models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.Object_Code)
 
 # table for recording bills under Salary Register (active register)
 # this register is accessed after filter ( pay, hra, da, etc) from the view as per input given by the user on the form
@@ -72,7 +103,6 @@ class Salary_Register(models.Model):
     Bill_Expenditure=models.IntegerField(default=0)
     Bill_Balance=models.IntegerField(default=0)
     Bill_Remark=models.CharField(default="",max_length=250)
-
 
 
   
