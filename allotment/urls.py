c506acd33fc0ea8108321e7b96a31e2953f819f7
unknown <abhishektiwari342123@gmail.com>
2022-03-30 17:03:49 +0530

    admin panel partially integrated

--- allotment/urls.py
+++ allotment/urls.py
@@ -9,6 +9,31 @@ urlpatterns= [
     # home after login
     path('home/', views.home, name='home'),
     path('home/salary-register/', views.salaryRegister, name='salary-register'),
-    path('admin1/home/', views.adminHome, name="admin-home")
+    path('admin1/home/', views.adminHome, name="admin-home"),
     
+    # 1st dropdown under Major
+    path('admin1/home/majorSub/', views.adminMajorSub, name="majorSub"),
+    
+    # 2nd dropdown under Major
+    path('admin1/home/minor/', views.adminMinor, name="minor"),
+
+    # 1st dropdown under Scheme
+    path('admin1/home/scheme/', views.adminScheme, name="scheme"),
+
+    # 2nd dropdown under Scheme
+    path('admin1/home/subScheme/', views.adminSubScheme, name="subScheme"),
+
+    # 1st dropdown under Mapping
+    path('admin1/home/minor-scheme/', views.adminMinor_Scheme, name="minor-scheme"),
+    
+    # 2nd dropdown under Mapping
+    path('admin1/home/scheme-subScheme/', views.adminSubScheme_Scheme, name="scheme-subScheme"),
+    
+
+    path('admin1/home/object/', views.adminObject, name="object"),
+
+    path('admin1/home/allotment/', views.adminAllotment, name="allotment"),
+
+    
+
 ]
--- allotment/views.py
+++ allotment/views.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
@@ -49,4 +50,85 @@ def salaryRegister(request):
 def adminHome(request):
     return render(request,'admin/adminBase.html')
 
-  
+def adminMajorSub(request):
+    if request.method=='POST':
+        print(request.POST)
+        newentry=Major_Sub_Head(Head_Code=request.POST.get('majorCode'),SubHead_Code=request.POST.get('SubCode'),
+        Head_Name=request.POST.get('majorName'),SubHead_Name=request.POST.get('SubName'))
+        newentry.save()
+        return redirect('majorSub')
+    else:
+        heads=Major_Sub_Head.objects.all()
+        return render(request,'admin/adminMajorSub.html',{'heads':heads})
+
+def adminMinor(request):
+    if request.method=='POST':
+        print(request.POST)
+        newentry=Minor_Head(MinorHead_Code=request.POST.get('minorCode'),
+        MinorHead_Name=request.POST.get('minorName'),
+        Head_Code=Major_Sub_Head.objects.get(pk=request.POST.get('head')))
+        newentry.save()
+        return redirect('minor')
+    else:
+        headCodes=Major_Sub_Head.objects.all()
+        print(headCodes[0])
+        print(headCodes[0].Head_Code)
+        minors=Minor_Head.objects.all()
+        return render(request,'admin/adminMinor.html',{'headCodes':headCodes,'minors':minors})
+
+def adminScheme(request):
+    if request.method=='POST':
+        pass
+    else:
+        return render(request,'admin/adminScheme.html')
+
+def adminSubScheme(request):
+    if request.method=='POST':
+        pass
+    else:
+        return render(request,'admin/adminSubScheme.html')
+
+# Mapping 1 (Scheme to Heads)        
+def adminMinor_Scheme(request):
+    if request.method=='POST':
+        pass
+    else:
+        heads=Minor_Head.objects.all()
+        schemes=Scheme.objects.all()
+        return render(request,'admin/adminMap-MinorScheme.html',{'heads':heads,'schemes':schemes})
+
+# Mapping 2 (sub Scheme to Scheme)             
+def adminSubScheme_Scheme(request):
+    if request.method=='POST':
+        pass
+    else:
+        schemes=Scheme.objects.all()
+        subSchemes=Sub_Scheme.objects.all()
+        print(subSchemes[2].SubScheme_Name)
+        return render(request,'admin/adminMap-Scheme_SubScheme.html',{'subSchemes':subSchemes,'schemes':schemes})
+
+def adminObject(request):
+    if request.method=='POST':
+        print(request.POST)
+        newentry=Object_Head(Object_Code=request.POST.get('objectCode'),Object_Name=request.POST.get('objectName'),
+        SubScheme_Code=Scheme_SubScheme.objects.get(id=request.POST.get('scheme')))
+        newentry.save()
+        return redirect('object')
+    else:
+        objects=Object_Head.objects.all()
+        headSubSchemes=Scheme_SubScheme.objects.all()
+        return render(request,'admin/adminObject.html',{'headSubSchemes':headSubSchemes,'objects':objects})
+
+
+
+def adminAllotment(request):
+    if request.method=='POST':
+        print(request.POST)
+        newEntry=Salary_Allotment(Object_Code=Object_Head.objects.get(id=request.POST.get('head')),
+        Allotment_Amount=request.POST.get('amount'))
+        newEntry.save()
+        return redirect('allotment')
+    else:
+        objects=Object_Head.objects.all()
+        allotments=Salary_Allotment.objects.all()
+        return render(request,'admin/adminAllotment.html',{'objects':objects,'allotments':allotments})
